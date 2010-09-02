ca58b2cded059d06490abad0cbf4c684057d5b94
Kieren Diment <diment@gmail.com>
2010-09-02 00:19:55 +0000

    Import of ZARQUON/Catalyst-Controller-reCAPTCHA-0.7 from CPAN.
    gitpan-cpan-distribution: Catalyst-Controller-reCAPTCHA
    gitpan-cpan-version:      0.7
    gitpan-cpan-path:         ZARQUON/Catalyst-Controller-reCAPTCHA-0.7.tar.gz
    gitpan-cpan-author:       ZARQUON
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Controller/reCAPTCHA.pm
+++ lib/Catalyst/Controller/reCAPTCHA.pm
@@ -4,7 +4,7 @@ use warnings;
 use base 'Catalyst::Controller';
 use Captcha::reCAPTCHA;
 use Carp 'croak';
-our $VERSION = '0.6';
+our $VERSION = '0.7';
 
 
 sub captcha_get : Private {
@@ -46,7 +46,6 @@ sub captcha_check : Private {
             unless $result->{is_valid};
 
     $c->stash->{recaptcha_ok} = 1 if $result->{is_valid};
-    $result->{is_valid} == $result->{is_valid};
     return 1;
 }
 
